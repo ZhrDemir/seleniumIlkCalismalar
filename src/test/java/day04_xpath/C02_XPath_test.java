d5fdcca5565570fc3d62bccc0cde651b3ae37fad
ZhrDemir <zdemir2702@gmail.com>
2023-11-17 11:37:52 +0300

    20231117

--- src/test/java/day04_xpath/C02_XPath_test.java
+++ src/test/java/day04_xpath/C02_XPath_test.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C01_Xpath {
+public class C02_XPath_test {
     public static void main(String[] args) throws InterruptedException {
 
 
@@ -24,11 +24,11 @@ public class C01_Xpath {
 
         // 2- Add Element butonuna basin
 
-        driver.findElement(By.xpath("//button[@id='sub-btn']")).click();
+        driver.findElement(By.xpath("//button[text()='Add']")).click();
 
         // 3- Delete butonu’nun gorunur oldugunu test edin
 
-        WebElement removeElementi=driver.findElement(By.xpath("//button[@class='remove-btn']"));
+        WebElement removeElementi=driver.findElement(By.xpath("//button[text()='Remove']"));
         if (removeElementi.isDisplayed()){
             System.out.println("Delete butonu görünürlüğü testi PASSED");
         }else System.out.println("Delete butonu görünürlüğü testi Failed");
@@ -39,19 +39,20 @@ public class C01_Xpath {
 
         // 5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
 
-        WebElement AddRemoveElementi=driver.findElement(By.xpath("//*[.='Add/Remove Elements']"));
+        WebElement AddRemoveElementi=driver.findElement(By.xpath("//*[text()='Add/Remove Elements']"));
+
         if (AddRemoveElementi.isDisplayed()){
             System.out.println("Add/Remove yazısı görünürlüğü testi PASSED");
         }else System.out.println("Add/Remove yazısı görünürlüğü testi Failed");
 
         //Remove butonunun görünür olmadığını test edin
-try {
-    removeElementi = driver.findElement(By.xpath("//button[@class='remove-btn']"));
-    System.out.println("Remove butonu görünmeme testi PASSED");
-}catch (NoSuchElementException e){
-    System.out.println("Remove butonu görünmeme testi Failed");
-    driver.quit();
-}
+        try {
+            removeElementi = driver.findElement(By.xpath("//button[.='Remove']"));
+            System.out.println("Remove butonu görünmeme testi FAİLED");
+        }catch (NoSuchElementException e) {
+            System.out.println("Remove butonu görünmeme testi PASSED");
+        }    driver.quit();
+
 
 
 
